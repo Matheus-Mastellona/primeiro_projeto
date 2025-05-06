56ac45fa9065b83384158987eacb5413d7c09890
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-06 16:55:29 -0300

    Começo das classes

--- projetoEstoque/src/main/java/com/mastelloni/projetoEstoque/model/Categoria.java
+++ projetoEstoque/src/main/java/com/mastelloni/projetoEstoque/model/Categoria.java
@@ -1,5 +1,6 @@
 package main.java.com.mastelloni.projetoEstoque.model;
 
 public class Categoria {
+    private String nome;
     
 }
--- projetoEstoque/src/main/java/com/mastelloni/projetoEstoque/model/Produto.java
+++ projetoEstoque/src/main/java/com/mastelloni/projetoEstoque/model/Produto.java
@@ -1,5 +1,36 @@
 package main.java.com.mastelloni.projetoEstoque.model;
 
 public class Produto {
-    
+    private String nome;
+    private double preco;
+    Estoque quantidadeEstoque;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidadeEstoque(Estoque quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Estoque getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
 }
